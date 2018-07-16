e7666be39aedd44141b6ad9d3b04555eb9a77399
Chelsea David <cryan17@cnm.edu>
2018-07-16 16:22:35 -0600

    created basic functions in javascript

---
+++ functions/js-functions.js
@@ -0,0 +1,32 @@
+// This will be the math function
+var m = myAge(4016,3987);
+console.log(m);
+function myAge(a,b){
+	return a - b;
+}
+
+//This will be the string function
+
+
+var p1 = 'She';
+var p2 = 'Sells';
+var p3 = 'Sea-Shells';
+var p4 ='by';
+var p5 = 'the';
+var p6 = 'sea' ;
+var p7 = 'shore';
+
+var combine =
+	String(p1) + " " +
+	String(p2) + " " +
+	String(p3) + " " +
+	String(p4) + " " +
+	String(p5) + " " +
+	String(p6) + " " +
+	String(p7) + ".";
+console.log(combine);
+
+// This will be the array function
+
+var myDogs = ['Opie', 'Chuck', 'Bently', 'Josie', 'Murphy'];
+console.log(myDogs[1] + " is the krustiest little mop in the whole neighborhood.");
